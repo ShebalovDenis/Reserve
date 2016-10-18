77869ac3c3447d6965393efe8865cf938d2fcec3
Vadim <wadim-ka95@mail.ru>
2016-10-18 18:07:03 +0400

    Шейкер готов к использованию

--- src/Shaker.java
+++ src/Shaker.java
@@ -3,13 +3,17 @@ public class Shaker {
 	public static int[] shaker(int[] arr) {
 		int l = 0;
 		int r = arr.length - 1;
+		while (r > l) {
 		for(int i = 0; i < r; i++) {
 			if (arr[i] >= arr[i+1]) 
 			arr[i]=arr[i]+arr[i+1]-(arr[i+1]=arr[i]);
 		}
+		r--;
 		for(int j = r ; j > l; j--) {
 			if (arr[j-1] >= arr[j]) 
 				arr[j-1]=arr[j-1]+arr[j]-(arr[j]=arr[j-1]);
+		}
+		l++;
 		}
 		return arr;
 	}
