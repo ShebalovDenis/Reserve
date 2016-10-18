6476af2729641c60360b3ee3a91607bbb6eca33c
Vadim <wadim-ka95@mail.ru>
2016-10-18 18:02:14 +0400

    Создан метод сортировки пузырьком.

--- src/Bubble.java
+++ src/Bubble.java
@@ -2,7 +2,10 @@
 public class Bubble {
 	public static int[] bubble(int[] arr) {
 		for(int i = arr.length - 1; i >= 0 ; i--) {
-			
+			for(int j = 0; j < arr.length - 1; j++) {
+				if (arr[j] >= arr[j+1]) 
+					arr[j]=arr[j]+arr[j+1]-(arr[j+1]=arr[j]); 
+				}
 		}
 		return arr;
 	}
