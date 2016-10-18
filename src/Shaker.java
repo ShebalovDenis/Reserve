9d64772be2b1c6c385816b081ea7aacfbc58a11f
Vadim <wadim-ka95@mail.ru>
2016-10-18 18:06:17 +0400

    Использовал левую и правую границу с помощью пузырька

--- src/Shaker.java
+++ src/Shaker.java
@@ -1,4 +1,16 @@
 
 public class Shaker {
-
+	public static int[] shaker(int[] arr) {
+		int l = 0;
+		int r = arr.length - 1;
+		for(int i = 0; i < r; i++) {
+			if (arr[i] >= arr[i+1]) 
+			arr[i]=arr[i]+arr[i+1]-(arr[i+1]=arr[i]);
+		}
+		for(int j = r ; j > l; j--) {
+			if (arr[j-1] >= arr[j]) 
+				arr[j-1]=arr[j-1]+arr[j]-(arr[j]=arr[j-1]);
+		}
+		return arr;
+	}
 }
